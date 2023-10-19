db457fca5cab931e863e131393e7d00c0645f498
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-10-19 06:56:38 -0600

    fixed setup-docs to filter images

--- classes/rename-me/setup-docs.sh
+++ classes/rename-me/setup-docs.sh
@@ -2,13 +2,17 @@
 
 basename=$(basename "$PWD")
 
-docker run --rm -v "$(pwd)":/documents/ asciidoctor/docker-asciidoctor asciidoctor-revealjs -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 main.adoc
+docker run --rm -v "$(pwd)":/documents/ -v "$(pwd)"/chapters:/documents/chapters -v "$(pwd)"/images:/documents/images \
+asciidoctor/docker-asciidoctor:1.55 asciidoctor-revealjs -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js main.adoc
 
 mv main.html $basename.html
 
-docker run --rm -v "$(pwd)":/documents/ asciidoctor/docker-asciidoctor asciidoctor -b html lab_book.adoc
+docker run --rm -v "$(pwd)":/documents/ -v "$(pwd)"/labs:/documents/labs -v "$(pwd)"/images:/documents/images \
+asciidoctor/docker-asciidoctor:1.55 asciidoctor -b html lab_book.adoc
+
+image_text_list=$(grep -roh "images/[^\"]*" *.html | sort | uniq | xargs -I % echo "./$basename/%" | tr '\n' ' ')
+image_array_list=(${(s: :)image_text_list})
 
 pushd ..
-zip $basename/$basename.zip -r $basename/images $basename/$basename.html $basename/*.css $basename/lab_book.html
+zip -r "$basename/$basename.zip" ${image_array_list} $basename/$basename.html $basename/*.css $basename/lab_book.html
 popd || exit
-
