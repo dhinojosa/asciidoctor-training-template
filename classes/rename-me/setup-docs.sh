291b2bfa75c590690576ae18381baa4ebf3240ab
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2024-07-01 09:38:15 -0600

    Add code to remove existing zip before compression
    The update introduces a line of code that deletes any existing zip file with the same name before proceeding with the compression. This is meant to prevent any potential conflicts or issues, allowing a fresh zip file to be created each time the script is run.

--- classes/rename-me/setup-docs.sh
+++ classes/rename-me/setup-docs.sh
@@ -13,6 +13,8 @@ asciidoctor/docker-asciidoctor:1.55 asciidoctor -b html lab_book.adoc
 image_text_list=$(grep -roh "images/[^\"]*" *.html | sort | uniq | xargs -I % echo "./$basename/%" | tr '\n' ' ')
 image_array_list=(${(s: :)image_text_list})
 
+rm -f $basename.zip
+
 pushd ..
 zip -r "$basename/$basename.zip" ${image_array_list} $basename/$basename.html $basename/*.css $basename/lab_book.html
 popd || exit
